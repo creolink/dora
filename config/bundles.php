fe1e35f26bd2d572d281872bceb986ef74be275a
Jakub Luczynski <jakub@billie.io>
2023-03-26 16:51:47 +0200

    symfony fixed

--- config/bundles.php
+++ config/bundles.php
@@ -2,6 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
-    SymfonyBundles\JsonRequestBundle\JsonRequestBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
