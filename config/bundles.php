728ca745d4ce512a4d6f92e967dceb4e833a074a
Jakub Luczynski <jakub@billie.io>
2023-03-26 16:43:00 +0200

    config fixes to stable

--- config/bundles.php
+++ config/bundles.php
@@ -3,7 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     SymfonyBundles\JsonRequestBundle\JsonRequestBundle::class => ['all' => true],
 ];
